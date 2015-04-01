45374dc83fa6b412fa74497a8d75cf4e9ce2d2f4
Nicolas Eckhart <nicolas.eckhart@renuo.ch>
2015-04-01 10:02:07 +0200

    replaced begin-end-while with kernel#loop with break (rubocop)

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -4,8 +4,9 @@ class ApiKey < ActiveRecord::Base
   private
 
   def generate_access_token
-    begin
+    loop do
       self.access_token = SecureRandom.hex
-    end while self.class.exists?(access_token: access_token)
+      break unless self.class.exists?(access_token: access_token)
+    end
   end
 end
