2e499788497ab0933e07a64c9e4ffa6e005d822d
Nicolas Eckhart <nicolas.eckhart@renuo.ch>
2015-04-08 16:25:01 +0200

    added a pre-commit hook and the brakeman gem

---
+++ pre-commit.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+
+rubocop
+RC=$?
+if [ ! $RC -eq 0 ]; then
+  echo 'COMMIT ABORTED: Rubocop has detected one or more offences, please review the code and re-commit your changes.'
+  exit 1
+fi
+
+brakeman
+RC=$?
+if [ ! $RC -eq 0 ]; then
+  echo 'COMMIT ABORTED: Brakeman has detected one or more security vulnerabilities, please review them and re-commit your changes.'
+  exit 1
+fi
