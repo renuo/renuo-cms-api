8f024b9dff6af8b70bc691304c5616b3a6397583
Nicolas Eckhart <nicolas.eckhart@renuo.ch>
2015-06-10 14:19:34 +0200

    removed unused spaces

--- spec/models/content_blocks_spec.rb
+++ spec/models/content_blocks_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe ContentBlock, type: :model do
   let(:content_block) { create(:content_block) }
 
   it 'reads a content_blocks content' do
-    expect(content_block.content).to be_truthy
+    expect(content_block.content).to_not be_falsey
   end
 end
