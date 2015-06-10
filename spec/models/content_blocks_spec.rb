b0448311fb7dda42f526fb78964389e0f2c17016
Nicolas Eckhart <nicolas.eckhart@renuo.ch>
2015-06-10 14:33:24 +0200

    added some more tests

--- spec/models/content_blocks_spec.rb
+++ spec/models/content_blocks_spec.rb
@@ -1,9 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe ContentBlock, type: :model do
-  let(:content_block) { create(:content_block) }
-
   it 'reads a content_blocks content' do
+    content_block = create(:content_block)
     expect(content_block.content).to_not be_falsey
+  end
+
+  it 'checks whether a content_blocks content can be edited' do
+    content_block = create(:content_block)
+    expect(content_block.content).to_not eq('foo bar')
+    content_block.update_attribute(:content, 'foo bar')
+    expect(content_block.content).to eq('foo bar')
+  end
+
+  it 'checks whether a content_block can be deleted' do
+    create(:content_block)
+    expect(ContentBlock.count).to eq(1)
+    ContentBlock.first.destroy
+    expect(ContentBlock.count).to eq(0)
+  end
+
+  it 'creates multiple content_blocks' do
+    expect(ContentBlock.count).to eq(0)
+    5.times { create(:content_block) }
+    expect(ContentBlock.count).to eq(5)
   end
 end
