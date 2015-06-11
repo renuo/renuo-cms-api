fd31d7969ffc8de64b7892e6cd464dee842c6297
Diego P. Steiner <diego.steiner@renuo.ch>
2015-06-11 14:28:04 +0200

    application setup

--- spec/factories/content_blocks.rb
+++ spec/factories/content_blocks.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :content_block, class: ContentBlock do
+    sequence(:content_path) { |i| "path-#{i}" }
     content "<h1>#{Faker::Lorem.word}</h1><p>#{Faker::Lorem.paragraph}<br/>#{Faker::Lorem.paragraph}</p>"
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,6 +14,13 @@ RSpec.configure do |config|
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
   end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
   config.include FactoryGirl::Syntax::Methods
   config.include Devise::TestHelpers, type: :controller
   config.include Capybara::DSL
