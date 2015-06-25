2f0ddb8d02b3cf8882a60bb420637ccae1cce5ef
Josua Schmid <josua.schmid@renuo.ch>
2015-06-25 14:57:02 +0200

    added route tests

--- spec/routing/content_blocks_routing_spec.rb
+++ spec/routing/content_blocks_routing_spec.rb
@@ -2,8 +2,16 @@ require 'rails_helper'
 
 RSpec.describe 'routes for ContentBlocks', type: :routing do
   it 'routes /content_blocks not to the content_blocks controller' do
-    expect(get: '/content_blocks').not_to be_routable
+    expect(get: '/api/content_blocks').not_to be_routable
   end
+
+  it 'routes the typical requests' do
+    expect(post: '/api/content_blocks/apikey').to be_routable
+    expect(get: '/api/content_blocks/apikey/1').to be_routable
+    expect(put: '/api/content_blocks/apikey/1').to be_routable
+    expect(delete: '/api/content_blocks/apikey/1').to be_routable
+  end
+
 end
 
 
