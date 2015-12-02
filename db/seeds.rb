5b82ec95d689aa74ccbe0216bbd8b142f46d50fb
Lukas Elmer <lukas.elmer@gmail.com>
2015-12-02 23:29:07 +0100

    more demo data

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,9 @@ ContentBlock.create!(content_path: 'some/path/to/some/content',
                      api_key: 'aValidApiKey')
 ContentBlock.create!(content_path: 'content/blub', content: '<strong>yay</strong> it works!',
                      api_key: 'aValidApiKey')
+ContentBlock.create!(content_path: 'even-more-content', content: '<strong>yay</strong> it works!',
+                     api_key: 'aValidApiKey')
+
+CredentialPair.create!(api_key: 'anotherPublicKey', private_api_key: 'SuperAdminOnly')
+ContentBlock.create!(content_path: 'content/blub', content: 'It also works :D',
+                     api_key: 'anotherPublicKey')
