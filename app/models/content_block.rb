d2dfa81cd3a7000831ccc5c08a99e902be3179af
Lukas Elmer <lukas.elmer@gmail.com>
2015-12-05 00:04:11 +0100

    remove content validation so empty contents may be saved

--- app/models/content_block.rb
+++ app/models/content_block.rb
@@ -1,5 +1,4 @@
 class ContentBlock < ActiveRecord::Base
   validates :api_key, presence: true
-  validates :content, presence: true
   validates :content_path, presence: true, length: { maximum: 1800 }
 end
