816c101d158c600cd85b7db5d20b82cb6f673fee
Lukas Elmer <lukas.elmer@gmail.com>
2016-01-04 05:47:56 +0100

    adjust seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,7 @@
-CredentialPair.create!(api_key: 'aValidApiKey', private_api_key: 'AdminONLY')
-ContentBlock.create!(content_path: 'some/path/to/some/content',
-                     content: 'this is the content of <strong>some/path/to/some/content</stong>',
-                     api_key: 'aValidApiKey')
-ContentBlock.create!(content_path: 'content/blub', content: '<strong>yay</strong> it works!',
-                     api_key: 'aValidApiKey')
-ContentBlock.create!(content_path: 'even-more-content', content: '<strong>yay</strong> it works!',
-                     api_key: 'aValidApiKey')
+CredentialPair.create!(api_key: 'aValidApiKey', private_api_key: 'AdminONLY', project_name: 'some-project')
+ContentBlock.create!(api_key: 'aValidApiKey', content_path: 'some/path/to/some/content', content: 'this is the content of <strong>some/path/to/some/content</stong>')
+ContentBlock.create!(api_key: 'aValidApiKey', content_path: 'content/blub', content: '<strong>yay</strong> it works!',)
+ContentBlock.create!(api_key: 'aValidApiKey', content_path: 'even-more-content', content: '<strong>yay</strong> it works!',)
 
-CredentialPair.create!(api_key: 'anotherPublicKey', private_api_key: 'SuperAdminOnly')
-ContentBlock.create!(content_path: 'content/blub', content: 'It also works :D',
-                     api_key: 'anotherPublicKey')
+CredentialPair.create!(api_key: 'anotherPublicKey', private_api_key: 'SuperAdminOnly', project_name: 'some-other-project')
+ContentBlock.create!(api_key: 'anotherPublicKey', content_path: 'content/blub', content: 'It also works :D')
