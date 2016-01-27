aa14ddb3d62ad945939b689f57dc2b0ee2d35315
Lukas Elmer <lukas.elmer@gmail.com>
2016-01-27 14:04:11 +0100

    refactoring

--- app/services/renuo_upload_credentials_service.rb
+++ app/services/renuo_upload_credentials_service.rb
@@ -1,6 +1,6 @@
 class RenuoUploadCredentialsService
   def credentials(api_key, private_api_key)
-    pair = CredentialPair.find_by api_key: api_key, private_api_key: private_api_key
+    pair = CredentialPair.find_by! api_key: api_key, private_api_key: private_api_key
     { api_key: pair.renuo_upload_api_key, signing_url: pair.renuo_upload_signing_url }
   end
 end
