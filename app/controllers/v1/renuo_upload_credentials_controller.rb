dd943fa74ab4b7e837569533d9270a435abbbfc2
Lukas Elmer <lukas.elmer@gmail.com>
2016-01-27 12:06:19 +0100

    implement credential verification for renuo upload credentials controller

--- app/controllers/v1/renuo_upload_credentials_controller.rb
+++ app/controllers/v1/renuo_upload_credentials_controller.rb
@@ -1,8 +1,15 @@
 module V1
   class RenuoUploadCredentialsController < ApplicationController
+    before_action :verify_key_pair
+
     def index
       service = RenuoUploadCredentialsService.new
       render json: { renuo_upload_credentials: service.credentials(params[:api_key], params[:private_api_key]) }
+    end
+
+    def verify_key_pair
+      pair_exists = CredentialPair.exists?(private_api_key: params[:private_api_key], api_key: params[:api_key])
+      head :unauthorized unless pair_exists
     end
   end
 end
--- spec/controllers/renuo_upload_credentials_spec.rb
+++ spec/controllers/renuo_upload_credentials_spec.rb
@@ -13,6 +13,21 @@ RSpec.describe V1::RenuoUploadCredentialsController, type: :controller do
         expect(object.api_key).to eq(credential_pair.renuo_upload_api_key)
         expect(object.signing_url).to eq(credential_pair.renuo_upload_signing_url)
       end
+
+      it 'blocks when the private api key is non-existent' do
+        credential_pair = create(:credential_pair)
+        unauthorized_api_params = { api_key: credential_pair.api_key, private_api_key: 'non-existent-private-api-key' }
+        get :index, unauthorized_api_params
+        expect(response).to have_http_status(:unauthorized)
+        expect(response.body).to eq('')
+      end
+
+      it 'does not find a resource' do
+        invalid_authorized_api_params = { api_key: 'non-exist', private_api_key: 'whatever' }
+        get :index, invalid_authorized_api_params
+        expect(response).to have_http_status(:unauthorized)
+        expect(response.body).to eq('')
+      end
     end
   end
 end
