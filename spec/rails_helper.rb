ba1daa5665ffd7725800fc1020d38acb12682ef7
Josua Schmid <josua.schmid@renuo.ch>
2016-07-11 11:36:24 +0200

    Require 100% SimpleCov coverage

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,10 @@ ENV['RAILS_ENV'] ||= 'test'
 require 'spec_helper'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
+require 'simplecov'
+
+SimpleCov.start 'rails'
+SimpleCov.minimum_coverage 100
 
 ActiveRecord::Migration.maintain_test_schema!
 
